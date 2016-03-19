fa933a51a30c2f93e30f195686d55cf233c42b8a
Joseph Hinkle <joehinkle11@gmail.com>
2016-03-19 02:11:40 -0400

    method to record audio

--- app/src/main/java/com/stephenk/audionotes/AudioRecordingHandler.java
+++ app/src/main/java/com/stephenk/audionotes/AudioRecordingHandler.java
@@ -1,7 +1,37 @@
 package com.stephenk.audionotes;
 
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.IOException;
+
 /**
  * Created by Joseph on 3/19/2016.
  */
 public class AudioRecordingHandler {
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static String mFileName = null;
+
+    private MediaRecorder mRecorder = null;
+    private MediaPlayer mPlayer = null;
+
+    public void startRecording(String audioFileName) {
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName += "/"+ audioFileName + ".3gp";
+
+
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
 }
